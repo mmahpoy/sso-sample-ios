27a70d6ee406b3a8f82c90b5e47f33864561dd6d
Darnell <gasolene@gmail.com>
2018-02-07 10:43:23 -0700

    Fix tests

--- sso-sample-iosTests/sso_sample_iosTests.swift
+++ sso-sample-iosTests/sso_sample_iosTests.swift
@@ -24,23 +24,25 @@ class sso_sample_iosTests: XCTestCase {
     func testThatObjectCanBeCreatedFromUrl() {
         let didParseUrl = expectation(description: "testThatObjectCanBeCreatedFromUrl")
         
-        let testToken = ""
+        let testToken = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkZW1vLnVzZXIyMDE4QHlvcG1haWwuY29tIiwiYXVkIjpbIm9wZW5pZCIsImFjY291bnQiLCJiaWxsaW5nIiwiYXR0IiwibzM2NSIsIm1hcmtldHBsYWNlIiwiY2hhbm5lbCIsImhvc3RlZGNoZWNrb3V0Iiwic2Vzc2lvbiIsInN1cGVydXNlciIsImludGVncmF0aW9uIiwicmVwb3J0aW5nIiwibm90aWZpY2F0aW9uIiwiUFJNIiwibWFya2V0cGxhY2V2MyJdLCJleHAiOjE1MTgwNjU4NzMsImp0aSI6ImQ3YjI4YmU4LWY2M2YtNGNmNi1hNTQ4LWU4ZTZlNmUyNWQzNyIsImNsaWVudF9pZCI6Im4xQkpPUkNnN3IiLCJzY29wZSI6WyJST0xFX1VTRVIiLCJvcGVuaWQiLCJwcm9maWxlIl19.Gb-q1DiEPn5y_088xmIKWiWuUepSAixdBftZeUY7QqIYFFKGQY5kitkG3dALRqoYBfBauUJ-45MAF2OOVrwvyMoZYWU9fsr-9N4JusS3FDhBWSK8deR8ETEYp7jaFdWQ6lEYZ2eHlKzbW0BBvMYTepZWw_HGpRdlJhk8C5gxm1HLGLinwJDHAqx8fcPDvlmn2lEIQlD5MM5u98-2ACP7J_UnAO4MP8cGBcPnPyjrInQniBqHPhbfPexbgZ1JWrQU71-8IzU1Wj-d80FNbZRscqzzwJts7zDWH9ckbg8nThPYNK6e-XGw1HCjhEq2d3byClm9JbTxp-0JwFsZ9IvQjA"
         
         if let userObject = UserObject.objectFromAccessToken(testToken) {
             
-            XCTAssertEqual(userObject.info["userName"] as? String, "kaligan@gmail.com")
-            XCTAssertEqual(userObject.info["password"] as? String, "*********")
-            XCTAssertNotNil(userObject.info["authorities"] as? [[String:AnyObject]])
+            XCTAssertEqual(userObject.info["sub"] as? String, "demo.user2018@yopmail.com")
+            XCTAssertEqual(userObject.info["client_id"] as? String, "n1BJORCg7r")
             
-            if let authorities = userObject.info["authorities"] as? [[String:AnyObject]] {
-                if authorities.count > 0 {
-                    XCTAssertEqual(authorities[0]["authority"] as? String, "ROLE_USER")
-                    didParseUrl.fulfill()
+            if let scope = userObject.info["scope"] as? [String] {
+                if scope.count > 0 {
+                    for role in scope {
+                        print(role)
+                        XCTAssertEqual(role, "ROLE_USER")
+                        didParseUrl.fulfill()
+                        break
+                    }
                 }
             }
         }
         
         waitForExpectations(timeout: 10, handler: nil)
     }
-    
 }
